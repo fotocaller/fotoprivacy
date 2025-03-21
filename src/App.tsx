4818552e582941101e24bb6d3c79eb3b06f5f377
Sahil <132002159+sevil-git@users.noreply.github.com>
2025-03-22 02:47:48 +0530

    added pp

---
+++ src/App.tsx
@@ -0,0 +1,79 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p>
+        Terms and Conditions
+        These terms and conditions (the “Agreement”) establish the comprehensive legal framework governing your access to and usage of the websitepolicies.com website (the “Website”) and its related products and services (collectively, the “Services”). This Agreement serves as a binding legal contract between you (referred to as “User,” “you,” or “your”) and Dynamic Terms Inc. (“Dynamic Terms Inc.,” “we,” “us,” or “our”). In the event you are accessing and using the Website or Services on behalf of a corporate entity, business organization, or any other legal entity, you hereby represent and warrant that you possess the necessary authority to bind such entity to the terms outlined in this Agreement. In such cases, references to “User,” “you,” or “your” shall refer to the entity you represent. If you lack the requisite authority or do not agree with the terms outlined in this Agreement, you are expressly prohibited from accepting this Agreement and from accessing or using the Website and Services. By continuing to use and access the Website and Services, you confirm that you have thoroughly reviewed, understood, and agreed to abide by the terms of this Agreement and privacy policy. It is important to recognize that this Agreement constitutes a legally binding contract between you and Dynamic Terms Inc., even though it is presented in an electronic format and does not require a physical signature.
+
+        Accounts and membership
+        If you choose to create an account on the Website, you accept full responsibility for maintaining the confidentiality and security of your account credentials, including your username and password. You also agree that you are solely responsible for all activities that occur under your account, including but not limited to any transactions, content submissions, or other actions undertaken using your account. Dynamic Terms Inc. reserves the right, but is under no obligation, to review new accounts before granting full access to the Website and Services. If you provide false or misleading contact information during the account registration process, we may terminate your account immediately and, in some cases, without prior notice. In the event of any unauthorized access to your account or any security breach, you must notify us promptly. Failure to do so may result in further unauthorized activity for which we will not be held liable. Dynamic Terms Inc. will not be responsible for any damages or losses incurred as a result of your failure to secure your account information. Additionally, we retain the right to suspend, disable, or permanently delete your account if we determine that your conduct or content violates any provision of this Agreement or is harmful to our reputation. If your account is terminated for these reasons, you are expressly prohibited from re-registering for the Services, and we may block your email address and IP address to prevent further attempts to access the Website and Services.
+
+        Billing and payments
+        You are required to pay all fees and charges associated with your account in accordance with the pricing and billing terms in effect at the time the payment is due. All pricing is listed in United States Dollars (USD) unless otherwise specified. Our Services are offered on a recurring subscription basis, which means that your subscription will automatically renew at the end of the billing cycle selected by you unless you cancel your subscription prior to the renewal date. By subscribing, you authorize us to charge the payment method on file at the recurring intervals selected by you. We offer a money-back guarantee for new purchases in accordance with our refund policy. If you are not satisfied with your purchase, you may request a refund within the specified time frame outlined in the refund policy. After the refund window has passed, no refunds will be provided, and the subscription will continue until canceled. You may cancel your subscription at any time through your account settings on the Website. If you choose to cancel, your subscription will remain active until the end of the current billing cycle, and you will not be charged for the subsequent renewal. If you do not cancel your subscription before the renewal date, it will automatically renew, and the applicable fees will be charged to your payment method. We may provide full or partial refunds if you choose to cancel your renewal after you were charged at our discretion.In the event that we determine a purchase constitutes a high-risk transaction, we reserve the right to request additional information to verify the transaction, such as a copy of a valid government-issued photo ID or a recent bank statement associated with the payment method used. This process is designed to ensure the security and integrity of all transactions conducted through our Website and to protect both our users and Dynamic Terms Inc. from fraudulent activity.
+
+        Accuracy of information
+        While we strive to ensure that all information on the Website is accurate and up to date, the information available on the Website may contain inaccuracies, omissions, or typographical errors. We reserve the right to correct any errors, inaccuracies, or omissions at any time, without prior notice, even after an order has been submitted. This may include canceling orders if the information pertaining to the purchase was incorrect at the time of submission. Additionally, we are not obligated to update or amend any outdated information on the Website, including but not limited to pricing information, except as required by law. It is important to note that the presence of an update or refresh date on the Website should not be interpreted as an indication that all information has been modified or is current.
+
+        Links to other resources
+        The Website and Services may contain links to external resources, such as third-party websites, applications, or other platforms. These links are provided solely for your convenience, and the inclusion of any link does not imply endorsement, sponsorship, approval, or affiliation with the linked resource unless explicitly stated. We do not have control over the content, accuracy, or availability of these external resources, nor do we assume responsibility for the offerings or actions of any businesses or individuals associated with these resources. You are responsible for reviewing the legal terms, conditions, and privacy policies of any external resources you access through the Website. Any use of third-party resources is done at your own risk, and Dynamic Terms Inc. shall not be held liable for any issues arising from your interaction with these resources.
+
+        Prohibited uses
+        In addition to the specific restrictions and limitations outlined elsewhere in this Agreement, you are prohibited from using the Website and Services in any manner that violates applicable local, national, or international laws and regulations. This includes, but is not limited to, engaging in activities that: (a) facilitate or promote unlawful conduct; (b) solicit others to commit illegal acts; (c) infringe upon or violate the intellectual property rights of others; (d) harass, abuse, or harm others based on race, gender, sexual orientation, religion, ethnicity, or disability; (e) spread false or misleading information; (f) distribute malware, viruses, or other harmful code; (g) engage in activities such as phishing, spamming, or scraping of data; (h) promote obscene, defamatory, or immoral behavior; or (i) circumvent security features of the Website, Services, or any related third-party products. We reserve the right to immediately terminate your access to the Website and Services if we determine that you have engaged in any prohibited activities or if your actions have otherwise violated the terms of this Agreement. Furthermore, certain features of our Services are advertised as unlimited and are subject to fair use, meaning they are provided without strict limits on usage but are intended for typical user behavior. Dynamic Terms Inc. reserves the right to monitor usage and, at its sole discretion, adjust or impose limits on unlimited features if it is determined that a user’s usage exceeds what is considered fair and reasonable or is otherwise detrimental to our services. These limits can be enforced or changed at any time, with or without notice, to ensure the integrity and availability of our Services for all users.
+
+        Intellectual property rights
+        Intellectual Property Rights refer to all legal rights associated with the protection of intellectual creations, including but not limited to copyrights, trademarks, patents, and design rights. These rights may be conferred by statute, common law, or equity, and may exist whether or not they are formally registered. Under this Agreement, no transfer of ownership or rights to any intellectual property owned by Dynamic Terms Inc. or third parties is granted to you. All rights, titles, and interests in such intellectual property shall remain exclusively with Dynamic Terms Inc. and its licensors. You are not authorized to use any of our trademarks, logos, or other proprietary materials without prior written consent. Unauthorized use of these materials constitutes a violation of this Agreement and may result in legal action.
+
+        Disclaimer of warranty
+        The Website and Services are provided on an “as is” and “as available” basis, without any warranties or guarantees, whether express or implied. We expressly disclaim any and all implied warranties, including but not limited to warranties of merchantability, fitness for a particular purpose, and non-infringement. We make no representations or warranties that the Services will meet your specific requirements, that they will be uninterrupted, timely, secure, or error-free, or that any defects in the Services will be corrected. Any content, data, or material downloaded or otherwise obtained through the Website is done at your sole discretion and risk, and you agree that you are fully responsible for any damage to your computer system or loss of data that results from such downloads. No information, whether oral or written, obtained from Dynamic Terms Inc. or through the Services shall create any warranty not expressly stated in this Agreement.
+
+        Limitation of liability
+        To the maximum extent permitted by applicable law, Dynamic Terms Inc., its affiliates, directors, officers, employees, agents, suppliers, and licensors shall not be held liable for any indirect, incidental, special, punitive, or consequential damages arising from or related to your use of the Website and Services. This includes, but is not limited to, damages for loss of profits, revenue, data, goodwill, or business opportunities, regardless of the legal theory under which the claim arises. Dynamic Terms Inc.’s total liability for any claims related to the Services shall be limited to the greater of one dollar ($1.00) or the amount you paid to Dynamic Terms Inc. for the Services in the one-month period preceding the event giving rise to the claim. These limitations apply even if the remedies provided herein fail to fulfill their essential purpose or if we were advised of the possibility of such damages in advance. See our disclaimer for further information on the limitation of our liability and the scope of our content.
+
+        Indemnification
+        You agree to indemnify, defend, and hold harmless Dynamic Terms Inc., its affiliates, directors, officers, employees, agents, suppliers, and licensors from any claims, liabilities, damages, losses, and expenses, including legal fees, arising from your use of the Website or Services, your breach of this Agreement, or your violation of any third-party rights, including but not limited to intellectual property rights.
+
+        Severability
+        If any provision of this Agreement is found to be invalid, illegal, or unenforceable by a court of competent jurisdiction, the remainder of the Agreement shall remain in full force and effect. The invalid or unenforceable provision will be replaced by a valid provision that most closely matches the intent of the original provision, ensuring that the overall purpose of the Agreement is preserved to the greatest extent possible.
+
+        Dispute resolution
+        This Agreement, along with any disputes or claims arising from or related to your use of the Website and Services, shall be governed by the laws of Ontario, Canada, without regard to its conflict of law provisions. Any legal proceedings arising out of this Agreement shall be exclusively brought in the courts located in Ontario, Canada, and you hereby submit to the jurisdiction of such courts. You waive your right to a jury trial in any legal dispute arising from this Agreement, and the United Nations Convention on Contracts for the International Sale of Goods shall not apply.
+
+        Assignment
+        You are prohibited from assigning, transferring, or delegating any of your rights or obligations under this Agreement without our prior written consent. Any unauthorized attempt to assign or transfer your rights shall be considered null and void. Dynamic Terms Inc. reserves the right to assign or transfer any or all of its rights and obligations under this Agreement, without your consent, as part of a sale of assets or shares, merger, or corporate restructuring.
+
+        Changes and amendments
+        Dynamic Terms Inc. reserves the right to modify or amend this Agreement at any time, and any changes will take effect immediately upon being posted on the Website unless otherwise specified. You will be notified of any significant changes through the contact information you have provided, if applicable, and your continued use of the Website following such modifications will constitute your acceptance of the revised Agreement. It is your responsibility to review the Agreement periodically for any changes.
+
+        Acceptance of these terms
+        By accessing and using the Website and Services, you acknowledge that you have read and agree to the terms and conditions set forth in this Agreement. If you do not agree to these terms, you are not authorized to use the Website or Services.
+
+        Contacting us
+        If you have any questions or concerns regarding this Agreement, you can contact us.
+
+        This document was last updated on September 7, 2024
+
+        RESOURCES
+        Blog
+        Perks and benefits
+        Data privacy laws
+        Legal glossary
+        Support
+        COMPANY
+        About us
+        Contact us
+        Pricing
+        Reviews
+        Partnerships
+        Press kit
+        WebsitePolicies logo
+        Dynamic Terms Inc. © 2025
+        Privacy Policy
+        Terms and Conditions
+      </p>
+    </div>
+  )
+}
+
+export default App
